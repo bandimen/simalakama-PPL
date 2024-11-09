f1e992802d976c394fea66ba9b130218c85e84b4
bandimen <fendiardianto10@gmail.com>
2024-11-09 18:03:03 +0700

    show modal perwalian

--- resources/views/pa/perwalian.blade.php
+++ resources/views/pa/perwalian.blade.php
@@ -168,9 +168,9 @@
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <!-- Modal header -->
-                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        {{ $i->nama }} - {{ $i->nim }}
+                <div class="flex items-center justify-center p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white text-center flex-grow">
+                        IRS {{ $i->nama }} - {{ $i->nim }} ({{ $i->status }})
                     </h3>
                     <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -210,6 +210,9 @@
                                     <th scope="col" class="px-6 py-3">
                                         Dosen Pengampu
                                     </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Aksi
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -242,7 +245,10 @@
                                         {{ $detail->status }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        Dosen Pengamppu
+                                        Dosen Pengampu
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#" title="delete"><img src="/images/icons/delete.png" alt="delete" class="h-4 w-4"></a>
                                     </td>
                                 </tr>
                                 @endforeach
