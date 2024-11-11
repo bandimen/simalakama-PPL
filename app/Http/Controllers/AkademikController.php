148f68c558dffdddd3b36bb9bfb99693b36073c1
Farrky <farreljoss9@gmail.com>
2024-11-11 12:59:11 +0700

    nambah beberapa untuk membuat akademik

---
+++ app/Http/Controllers/AkademikController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Akademik;
+use App\Http\Requests\StoreAkademikRequest;
+use App\Http\Requests\UpdateAkademikRequest;
+
+class AkademikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAkademikRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Akademik $akademik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Akademik $akademik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAkademikRequest $request, Akademik $akademik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Akademik $akademik)
+    {
+        //
+    }
+}
