a4adb6f01f8edd3944ae28e3d2e364ae79068824
Farrky <farreljoss9@gmail.com>
2024-11-11 09:25:01 +0700

    perubahan untuk akademik

---
+++ resources/views/akademik/dashboard.blade.php
@@ -0,0 +1,11 @@
+{{-- resources/views/dekan/tambah-mata-kuliah.blade.php --}}
+<x-layout>
+    <x-slot:title>Tambah Mata Kuliah</x-slot:title>
+    
+    <div class="min-h-full">
+        <x-navbar-akademik></x-navbar-akademik>
+        
+        <main>
+           
+    </div>
+</x-layout>
